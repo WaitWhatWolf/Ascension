9fca3f1a487e259dd51d851b59cfcaf353c9a211
Mariusz <aWhiteWarriorWolf@outlook.jp>
2021-08-05 19:04:43 +0200

    Changed StandType name to StandID

---
+++ Enums/StandID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ascension.Enums
+{
+    /// <summary>
+    /// Identification for Stands.
+    /// </summary>
+    public enum StandID
+    {
+        NEWBIE = -1,
+        STAR_PLATINUM = 0,
+        THE_WORLD,
+        MAGICIANS_RED,
+        KILLER_QUEEN,
+        HIEROPHANT_GREEN,
+    }
+}
