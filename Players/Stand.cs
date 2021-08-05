c21d1e8e6dc556537cd533845063643f317bf712
Mariusz <aWhiteWarriorWolf@outlook.jp>
2021-08-05 19:00:25 +0200

    Create Stand.cs

---
+++ Players/Stand.cs
@@ -0,0 +1,21 @@
+﻿using Ascension.Enums;
+
+namespace Ascension.Players
+{
+    /// <summary>
+    /// Core record for Stands.
+    /// </summary>
+    public sealed record Stand
+    {
+        public string Name { get; }
+        public StandID ID { get; }
+
+        public Stand(StandID id)
+        {
+            ID = id;
+            Name = ASCResources.Players.GetStandName(ID);
+        }
+    
+        public static implicit operator bool(Stand stand) => stand != null && stand.ID != StandID.NEWBIE;
+    }
+}
