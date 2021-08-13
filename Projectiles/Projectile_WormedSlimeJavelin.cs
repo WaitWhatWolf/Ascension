c12e943d7ce4836b6e71367c23a4004e5899b01f
Mariusz <aWhiteWarriorWolf@outlook.jp>
2021-08-13 17:55:06 +0200

    Started WormedSlime stuff

---
+++ Projectiles/Projectile_WormedSlimeJavelin.cs
@@ -0,0 +1,27 @@
+﻿using Ascension.Attributes;
+using Ascension.Enums;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Ascension.Items;
+
+namespace Ascension.Projectiles
+{
+    /// <summary>
+    /// Thrown by <see cref="Item_WormedSlimeJavelin"/>.
+    /// </summary>
+    [CreatedBy(Dev.WaitWhatWolf, "2021/08/11 18:06:15")]
+    public class Projectile_WormedSlimeJavelin : AscensionProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Wormed Slime Javelin");
+        }
+
+        public override void SetDefaults()
+        {
+
+        }
+    }
+}
---
+++ Projectiles/Projectile_WormedSlimeWorm.cs
@@ -0,0 +1,28 @@
+﻿using Ascension.Attributes;
+using Ascension.Enums;
+using Terraria.ModLoader;
+
+namespace Ascension.Projectiles
+{
+    /// <summary>
+    /// The base class to spawn a wormed slime projectile.
+    /// </summary>
+    [CreatedBy(Dev.WaitWhatWolf, 2021, 08, 11), Note(Dev.WaitWhatWolf, "Class is abstract generic to allow for different damage types, see first line in SetDefaults().")]
+    public abstract class Projectile_WormedSlimeWorm<T> : AscensionProjectile where T : DamageClass
+    {
+        public override string Name { get; } = "Projectile_WormedSlimeWorm";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Wormed Slime Worm");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = ModContent.GetInstance<T>();
+
+            Projectile.height = 4;
+            Projectile.width = 12;
+        }
+    }
+}
